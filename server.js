1275d8ea1aa12781f9feede54f1abe90871b3cb8
Spencer Du <spencer@wearelighten.co.uk>
2023-01-14 19:13:42 +0000

    make it better

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+mongoose
+  .connect(
+    process.env.MONGODB_URI ||
+      'mongodb+srv://sdf1444:boggie234@mongodb.z4e4t.mongodb.net/?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(function onSuccess() {
+    console.log('The server connected with MongoDB.');
+  })
+  .catch(function onError() {
+    console.log('Error while connecting with MongoDB.');
+  });
+
+/** Seting up server to accept cross-origin browser requests */
+app.use(function (req, res, next) {
+  //allow cross origin requests
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'POST, PUT, OPTIONS, DELETE, GET'
+  );
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// Define Routes
+app.use(cors({origin: '*'}));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+// // Serve static assets in production
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
