30062d2baeeb062a8de3f33ef4ef63114a6a188a
Spencer Du <spencer@wearelighten.co.uk>
2023-01-14 16:46:35 +0000

    add server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
 const express = require('express');
-const connectDB = require('./config/db');
-const bodyParser = require('body-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
-// Connect Database
-connectDB();
+mongoose
+  .connect(
+    process.env.MONGODB_URI ||
+      'mongodb+srv://sdf1444:boggie234@mongodb.z4e4t.mongodb.net/?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(function onSuccess() {
+    console.log('The server connected with MongoDB.');
+  })
+  .catch(function onError() {
+    console.log('Error while connecting with MongoDB.');
+  });
 
 /** Seting up server to accept cross-origin browser requests */
 app.use(function (req, res, next) {
@@ -24,10 +40,12 @@ app.use(function (req, res, next) {
 });
 
 app.use(bodyParser.json());
+app.use(logger('dev'));
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+// Define Routes
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -35,11 +53,12 @@ app.use(
 );
 
 // Define Routes
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
 // // Serve static assets in production
 // if (process.env.NODE_ENV === 'production') {
 //   // Set static folder
