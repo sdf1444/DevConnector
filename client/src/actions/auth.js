3933913a549366a2492eae71af734a8f50ec1197
Spencer Du <spencer@wearelighten.co.uk>
2023-01-14 06:13:38 +0000

    update api endpoints

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -14,7 +14,7 @@ import {
 // Load User
 export const loadUser = () => async (dispatch) => {
   try {
-    const res = await axios.get('/api/auth');
+    const res = await axios.get('https://dev-connector.herokuapp.com/api/auth');
 
     dispatch({
       type: USER_LOADED,
@@ -38,7 +38,7 @@ export const register = ({ name, email, password }) => async (dispatch) => {
   const body = JSON.stringify({ name, email, password });
 
   try {
-    const res = await axios.post('/api/users', body, config);
+    const res = await axios.post('https://dev-connector.herokuapp.com/api/users', body, config);
 
     dispatch({
       type: REGISTER_SUCCESS,
@@ -69,7 +69,7 @@ export const login = (email, password) => async (dispatch) => {
   const body = JSON.stringify({ email, password });
 
   try {
-    const res = await axios.post('/api/auth', body, config);
+    const res = await axios.post('https://dev-connector.herokuapp.com/api/auth', body, config);
 
     dispatch({
       type: LOGIN_SUCCESS,
