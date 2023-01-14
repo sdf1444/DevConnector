a9d995fac86c5336d664214bc345fd0ef4142a9c
Spencer Du <spencer@wearelighten.co.uk>
2023-01-14 06:23:45 +0000

    cross origin

--- server/server.js
+++ server/server.js
@@ -16,6 +16,22 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+/** Seting up server to accept cross-origin browser requests */
+app.use(function (req, res, next) {
+  //allow cross origin requests
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'POST, PUT, OPTIONS, DELETE, GET'
+  );
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
