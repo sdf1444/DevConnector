0f791cd9ab4ffcd5b5658efc7c8578b91cea65bb
Spencer Du <spencer@wearelighten.co.uk>
2023-01-14 20:13:27 +0000

    hello

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -2,30 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const ProfileItem = ({
-  profile: {
-    user: { _id = 'spencer', name = 'spencer', avatar = 'spencer' },
-    status,
-    company,
-    location,
-    skills
-  }
-}) => {
+const ProfileItem = (props) => {
   return (
     <div className='profile bg-light'>
-      <img src={avatar} alt='' className='round-img' />
+      <img src={props.profile.user.avatar} alt='' className='round-img' />
       <div>
-        <h2>{name}</h2>
+        <h2>{props.profile.user.name}</h2>
         <p>
-          {status} {company && <span> at {company}</span>}
+          {props.profile.status} {props.profile.company && <span> at {props.profile.company}</span>}
         </p>
-        <p className='my-1'>{location && <span>{location}</span>}</p>
-        <Link to={`/profile/${_id}`} className='btn btn-primary'>
+        <p className='my-1'>{props.profile.location && <span>{props.profile.location}</span>}</p>
+        <Link to={`/profile/${props.profile.user._id}`} className='btn btn-primary'>
           View Profile
         </Link>
       </div>
       <ul>
-        {skills.slice(0, 4).map((skill, index) => (
+        {props.profile.skills.slice(0, 4).map((skill, index) => (
           <li key={index} className='text-primary'>
             <i className='fas fa-check' /> {skill}
           </li>
@@ -35,8 +27,8 @@ const ProfileItem = ({
   );
 };
 
-ProfileItem.propTypes = {
-  profile: PropTypes.object.isRequired
-};
+// ProfileItem.propTypes = {
+//   props: PropTypes.object.isRequired
+// };
 
 export default ProfileItem;
