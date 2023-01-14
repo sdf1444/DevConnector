a11e0197b35bb0004e9fcecca13590072c668130
Spencer Du <spencer@wearelighten.co.uk>
2023-01-14 06:39:05 +0000

    cors

--- server/server.js
+++ server/server.js
@@ -1,20 +1,11 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require('path');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 // Connect Database
 connectDB();
-
-// Init Middleware
-app.use(express.json());
-
-// Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
 
 /** Seting up server to accept cross-origin browser requests */
 app.use(function (req, res, next) {
@@ -32,15 +23,32 @@ app.use(function (req, res, next) {
   next();
 });
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+app.use(bodyParser.json());
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// Define Routes
+app.use(cors());
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+// // Serve static assets in production
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 
