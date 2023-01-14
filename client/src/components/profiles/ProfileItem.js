7e3111a49eb628333890701957b73af18e4f8c85
Spencer Du <spencer@wearelighten.co.uk>
2023-01-14 19:31:16 +0000

    hello

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -9,7 +9,7 @@ const ProfileItem = (props) => {
       <div>
         {/* <h2>{name}</h2>  */}
         <p>
-          {props} {props.company && <span> at {props.company}</span>}
+          {status} {props.company && <span> at {props.company}</span>}
         </p>
         <p className='my-1'>{props.location && <span>{props.location}</span>}</p>
         {/* <Link to={`/profile/${_id}`} className='btn btn-primary'>
@@ -17,14 +17,14 @@ const ProfileItem = (props) => {
         </Link> */}
       </div>
       <ul>
-        {props.status}
+        {props.skills.slice(0, 4).map((skill, index) => (
+          <li key={index} className='text-primary'>
+            <i className='fas fa-check' /> {skill}
+          </li>
+        ))}
       </ul>
     </div>
   );
-};
-
-ProfileItem.propTypes = {
-  profile: PropTypes.object.isRequired
 };
 
 export default ProfileItem;
